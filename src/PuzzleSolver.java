9f5990e02d4f40afadccd55f2bfd23918d32e052
carllix <carloangkisan21@gmail.com>
2025-02-22 20:43:20 +0700

    feat: add PuzzleSolver class

---
+++ src/PuzzleSolver.java
@@ -0,0 +1,82 @@
+package src;
+
+import java.util.List;
+
+public class PuzzleSolver {
+    private Board board;
+    private List<Block> blocks;
+    private long totalCaseChecked = 0;
+    private long duration = 0;
+
+    public PuzzleSolver(Board board, List<Block> blocks) {
+        this.board = board;
+        this.blocks = blocks;
+    }
+
+    public long getTotalCaseChecked() {
+        return this.totalCaseChecked;
+    }
+
+    public long getDuration() {
+        return this.duration;
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
+
+    private boolean solve(int idx) {
+
+        if(board.isFull()) {
+            if (idx < blocks.size()) {
+                return false;
+            }
+            return true;
+        }
+
+        if (idx == blocks.size()) {
+            return false;
+        }
+
+        Block block = blocks.get(idx);
+        List<Block> allRotationsAndFlips = block.getAllRotationsAndFlips();
+        for (int i = 0; i < allRotationsAndFlips.size(); i++) {
+            Block cells = allRotationsAndFlips.get(i);
+            for (int x = 0; x < board.getRows(); x++) {
+                for (int y = 0; y < board.getCols(); y++) {
+                    if (board.canPlaceBlock(cells, x, y)) {
+                        totalCaseChecked++;
+                        board.placeBlock(cells, x, y);
+
+                        if (solve(idx + 1))
+                            return true;
+
+                        board.removeBlock(cells, x, y);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public void solvePuzzle() {
+        long startTime = System.currentTimeMillis();
+
+        boolean isSolved = solve(0);
+        if (!isSolved) {
+            System.out.println("Tidak ada solusi yang ditemukan.");
+        } else {
+            System.out.println("Solusi ditemukan.");
+            board.printBoard();
+        }
+        long endTime = System.currentTimeMillis();
+        this.duration = endTime - startTime;
+
+        System.out.println("Waktu pencarian: " + (this.duration) + " ms");
+        System.out.println("Banyak kasus yang ditinjau: " + this.totalCaseChecked);
+    }
+
+    public static void main(String[] args) {
+        // Testing
+    }
+}
